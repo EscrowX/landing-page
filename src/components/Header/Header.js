4df72603d68ec8c0acc10013633844cf9628847a
Tomasz Kowalczyk <tomek@kownet.info>
2021-08-14 17:46:29 +0200

    gtm added + noreferrer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,10 +29,10 @@ function Header() {
 				<nav className={isNavbarOpen ? `${nav} ${navOpened}` : nav}>
 					<ul className={navList}>
 						<li className={navItem}>
-							<a href="https://invis.io/YJ10RDVLZ87R" target="_blank">Demo</a>
+							<a href="https://invis.io/YJ10RDVLZ87R" target="_blank" rel="noreferrer">Demo</a>
 						</li>
 						<li className={navItem}>
-							<a href="https://faq.escrowx.io/" target="_blank">FAQ</a>
+							<a href="https://faq.escrowx.io/" target="_blank" rel="noreferrer">FAQ</a>
 						</li>
 						<li className={navItem}>
 							<a href="#">about us</a>
